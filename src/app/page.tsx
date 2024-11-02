f9e1575e8eaadf0cd19508de4ff4782afa69dd34
ofjangra <zannrez@gmail.com>
2024-11-02 17:00:02 +0530

    hero section development

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,12 @@ import { AvatarCirclesDemo } from "../components/ui/avatar-circlesUi";
 import ColorfulCardHover from "../components/ColorfulCardHover";
 import { MarqueeDemoVertical } from "@/components/ui/marqueeVertical";
 import { HomeBackground } from "../components/ui/Background";
+import { IoIosStar } from "react-icons/io";
+import Flogo from "../assets/FrontudeLogo.svg"
+import Image from "next/image";
+// import "./"
+
+
 
 const Home = () => {
   return (
@@ -16,7 +22,7 @@ const Home = () => {
         <section className="h-screen w-full flex items-center justify-center relative overflow-hidden">
           {/* Light ray effect */}
           <div
-            className="absolute top-0 left-1/2 -translate-x-1/2 h-[160%] pointer-events-none"
+            className="absolute top-0 left-1/2 -translate-x-1/2 h-[160%] opacity-50 pointer-events-none"
             style={{
               width: "30%",
               clipPath: "polygon(48% 0%, 52% 0%, 150% 100%, -50% 100%)",
@@ -32,8 +38,37 @@ const Home = () => {
               transform: "translateX(-50%) translateY(-15%)",
             }}
           />
-          <Globe className="!relative !h-[600px] z-10 translate-y-[30%]" />
-          {/* Adjust size and vertical position here */}
+          <Globe className="absolute !h-[600px] translate-y-[30%] opacity-50 -z-10" />
+          <div className="flex flex-col items-center">
+            <div className="flex items-start gap-6">
+              <AvatarCirclesDemo />
+              <div className="flex flex-col items-start justify-between">
+                <div className="flex items-center gap-2">
+                  <IoIosStar className="text-white" />
+                  <IoIosStar className="text-white" />
+                  <IoIosStar className="text-white" />
+                  <IoIosStar className="text-white" />
+                  <IoIosStar className="text-white" />
+                </div>
+                <p>100+ Businesses scaled</p>
+              </div>
+            </div>
+            <h1 className="text-white text-center">Turn your <span>business vision</span> <br />into digital masterpiece</h1>
+            <p className="mt-4 text-[20px]">
+              we bring your ideas to life, creating standout <br /> products that fuel your growth and success.
+            </p>
+          </div>
+          <div className="flex justify-center p-8 items-center absolute bottom-0 left-0 right-0">
+            <div className="flex items-center gap-[50px]">
+              <Image src={Flogo} alt="Frontude Logo" height={36} />
+              <Image src={Flogo} alt="Frontude Logo" height={36} />
+              <Image src={Flogo} alt="Frontude Logo" height={36} />
+              <Image src={Flogo} alt="Frontude Logo" height={36} />
+              <Image src={Flogo} alt="Frontude Logo" height={36} />
+              <Image src={Flogo} alt="Frontude Logo" height={36} />
+              <Image src={Flogo} alt="Frontude Logo" height={36} />
+            </div>
+          </div>
         </section>
 
         {/* Rest of the sections */}
